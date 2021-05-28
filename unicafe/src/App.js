49d44210cd4a204d4cfadee62377c8d6a12b5024
Teslim <teslimni@gmail.com>
2021-05-28 13:12:32 +0100

    Complete the unicafe assignment

---
+++ unicafe/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Button from './button'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allRatings, setAll] = useState([])
+  
+  // handle each event and save each click to the allRatings array 
+  const handleGoodClick = () => {
+    setAll(allRatings.concat(1))
+    setGood(good + 1)
+  } 
+  const handleNeutralClick = () => {
+    setAll(allRatings.concat(1))
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setAll(allRatings.concat(1))
+    setBad(bad + 1)
+  }
+// Sum the number of clicks registered in the allRatings array
+  const sumRatings = allRatings.reduce((a,b) => a + b, 0)
+  
+  return (
+    <div className="App">
+      <h1>
+        Give feedback here
+      </h1>
+  
+      <Button handleClick={handleGoodClick} text={good} />
+      <Button handleClick={handleNeutralClick} text={neutral} />
+      <Button handleClick={handleBadClick} text={bad} />
+      <h2>Statistics</h2>
+      good {good} <br/>
+      neutral {neutral}<br/>
+      bad {bad}<br/>
+      all {sumRatings} <br/>
+      average {sumRatings/3} <br/>
+      positive {good/100 * sumRatings}%
+    </div>
+  );
+}
+
+export default App;
---
+++ unicafe/src/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Button = ({handleClick, text}) => (
+     <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
+export default Button;
+
